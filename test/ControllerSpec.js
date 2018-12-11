3d39f64d5c3f221c749b1b3b5d94c8d49bb9327c
GaellePrchc <gaelle.perchoc@gmail.com>
2018-12-11 15:20:29 +0100

    completed tests

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -60,6 +60,12 @@ describe('controller', function () {
 
 	it('should show entries on start-up', function () {
 		// TODO: write test
+		var todo = {title: 'a todo'};									 // Define a todo
+		setUpModel([todo]);												 // Set up the model
+		
+		subject.setView('');											 // Set a view without a specific route
+
+		expect(view.render).toHaveBeenCalledWith('showEntries', [todo]); // Expect the view to call the "render" method with "showEntries" et the array of todos to display
 	});
 
 	describe('routing', function () {
@@ -84,10 +90,25 @@ describe('controller', function () {
 
 		it('should show active entries', function () {
 			// TODO: write test
+			var todo = {title: 'a todo', completed: false};											// Define an active todo
+			setUpModel([todo]);																		// Set up the model
+
+			subject.setView('#/active');															// Set the view with "active" route
+
+			expect(model.read).toHaveBeenCalledWith({completed: false}, jasmine.any(Function));		// Expect the model to call "read" method with the object "{completed: false}" and a callback function to return all active todos from db
+			expect(view.render).toHaveBeenCalledWith('showEntries', [todo]);						// Expect the view to call the "render" method with "showEntries" as parameter to display the todos
+
 		});
 
 		it('should show completed entries', function () {
 			// TODO: write test
+			var todo = {title:'a todo', completed: true};											// Define a completed todo
+			setUpModel([todo]);																		// Set up the model
+
+			subject.setView('#/completed');															// Set the view with "completed" route
+
+			expect(model.read).toHaveBeenCalledWith({completed: true}, jasmine.any(Function));		// Expect the model to call "read" method with the object "{completed: true}" and a callback function to return all completed todos from db
+			expect(view.render).toHaveBeenCalledWith('showEntries', [todo]);						// Expect the view to call the "render" method with "showEntries" as parameter to display the todos
 		});
 	});
 
@@ -135,25 +156,62 @@ describe('controller', function () {
 
 	it('should highlight "All" filter by default', function () {
 		// TODO: write test
+		var todo = {title: 'a todo'};								// Define a todo
+		setUpModel([todo]);											// Set up the model
+
+		subject.setView('');										// Set the view without a route
+		expect(view.render).toHaveBeenCalledWith('setFilter', '');	// Expect the view to call "render" method with "setFilter" and an empty string as parameters
+
 	});
 
 	it('should highlight "Active" filter when switching to active view', function () {
 		// TODO: write test
+		var todo = {title: 'a todo'};										// Define a todo
+		setUpModel([todo]);													// Set up the model
+
+		subject.setView('#/active');										// Set the view with an active route
+		expect(view.render).toHaveBeenCalledWith('setFilter', 'active');	// Expect the view to call "render" method with "setFilter" and "active" as parameters
 	});
 
 	describe('toggle all', function () {
 		it('should toggle all todos to completed', function () {
 			// TODO: write test
+			var todo = {id: 42, title: 'an active todo', completed: false};							 // Define an active todo
+
+			setUpModel([todo]);																		 // Set up the model
+			subject.setView('');																	 // Set the view without a specific view
+
+			view.trigger('toggleAll', {completed: true});											 // Call "toggleAll" event with the object "{completed: true}" as parameter		
+			
+			expect(model.read).toHaveBeenCalledWith({completed: false}, jasmine.any(Function));		 // Expect the model to call "read" method with the object "{completed: false}" and a callback funtion to return all active todos from db
+			expect(model.update).toHaveBeenCalledWith(42, {completed: true}, jasmine.any(Function)); // Expect the model to call "update" method with the id of the todo, the object "{completed: false}" and a callback funtion to update the active todos as completed
+
 		});
 
 		it('should update the view', function () {
 			// TODO: write test
+			var todo = {id: 42, title: 'an active todo', completed: false};							// Define an active todo
+
+			setUpModel([todo]);																		// Set up the model																
+			subject.setView('');																	// Set the view without a specific route
+			
+			view.trigger('toggleAll', {completed: true});											// Call "toggleAll" event with the object "{completed: true}" as parameter	 
+
+			expect(view.render).toHaveBeenCalledWith('elementComplete', {id: 42, completed: true}); // Expect the view to call "render" method with "elementComplete" and an object as parameters
 		});
+
 	});
 
 	describe('new todo', function () {
 		it('should add a new todo to the model', function () {
 			// TODO: write test
+			setUpModel([]);																		// Set up the model without any todo
+
+			subject.setView('');																// Set the view without a specific route
+			
+			view.trigger('newTodo', 'a new todo');												// Call the "newTodo" event with a string as parameter
+			
+			expect(model.create).toHaveBeenCalledWith('a new todo', jasmine.any(Function));		// Expect the model to call "create" method with the title of the new todo and a callback function
 		});
 
 		it('should add a new todo to the view', function () {
@@ -194,6 +252,13 @@ describe('controller', function () {
 	describe('element removal', function () {
 		it('should remove an entry from the model', function () {
 			// TODO: write test
+			var todo = {id: 42, title: 'a todo'};									// Define a todo
+			setUpModel([todo]);														// Set up the model
+
+			subject.setView('');													// Set the view without a route
+			view.trigger('itemRemove', {id: 42});									// Call "itemRemove" event with the id of the todo to remove in parameter
+
+			expect(model.remove).toHaveBeenCalledWith(42, jasmine.any(Function));	// Expect the model to call "remove" method with '42', the ID of the remove todo, and a callback function
 		});
 
 		it('should remove an entry from the view', function () {
@@ -260,6 +325,7 @@ describe('controller', function () {
 
 			expect(view.render).toHaveBeenCalledWith('elementComplete', {id: 42, completed: false});
 		});
+
 	});
 
 	describe('edit item', function () {
